2d909f4b75127a00cdc15b7fead8271df0ada9e1
Maximilian Fischer <boehnsins@gmail.com>
2022-12-20 17:55:46 +0100

    add: device creation via http

--- src/rest/http_test.go
+++ src/rest/http_test.go
@@ -1,1 +1,15 @@
 package rest
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_createNewDeviceViaPostRequest(t *testing.T) {
+	rest := RESTInterface{}
+	assert := assert.New(t)
+	values, _ := url.ParseQuery("name=thermostat")
+	assert.HTTPSuccess(rest.SetNewDevice, "POST", "localhost:8000/devices", values, nil)
+}
