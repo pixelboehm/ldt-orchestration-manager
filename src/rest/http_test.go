0c1b18e4657e877eba8c06f4a4dc4e625337dec9
Maximilian Fischer <boehnsins@gmail.com>
2022-12-20 17:00:12 +0100

    remove dummy tests

--- src/rest/http_test.go
+++ src/rest/http_test.go
@@ -1,12 +1,1 @@
 package rest
-
-import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
-
-func TestPass(t *testing.T) {
-	assert := assert.New(t)
-	assert.Truef(true, "this test should pass")
-}
