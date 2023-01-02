03241f8bd034e867a749f6da413f011ffbd9426e
Maximilian Fischer <boehnsins@gmail.com>
2023-01-02 10:55:16 +0100

    reafactor test to use table driven approach

--- src/device/device_test.go
+++ src/device/device_test.go
@@ -8,8 +8,20 @@ import (
 
 func Test_createDevice(t *testing.T) {
 	assert := assert.New(t)
-	device := NewDevice("Some Name")
-	assert.NotNilf(device, "Unable to create Device")
+	var tests = []struct {
+		name  string
+		input string
+		want  device
+	}{
+		{"Device should be named Foo", "Foo", device{"Foo"}},
+		{"Device should be named 1234", "1234", device{"12344"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ans := NewDevice(tt.input)
+			assert.Equal(ans, tt.want)
+		})
+	}
 }
 
 func Test_getName(t *testing.T) {
