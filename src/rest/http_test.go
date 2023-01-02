14fcd73255c853ff911b8af914a2f767947de52c
Maximilian Fischer <boehnsins@gmail.com>
2023-01-02 17:29:25 +0100

    test for getDevice succeeds

--- src/rest/http_test.go
+++ src/rest/http_test.go
@@ -20,3 +20,9 @@ func Test_createNewDeviceWithEmptyNameViaPostRequest(t *testing.T) {
 	values, _ := url.ParseQuery("name=")
 	assert.HTTPError(rest.SetNewDevice, "POST", "localhost:8000/devices", values, nil)
 }
+
+func Test_GetDevicesSucceeds(t *testing.T) {
+	assert := assert.New(t)
+	rest := RESTInterface{}
+	assert.HTTPSuccess(rest.GetDevices, "GET", "localhost:8000/devices", nil, nil)
+}
