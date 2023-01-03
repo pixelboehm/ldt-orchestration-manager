8bcc47b69efa0a3b887d9573e27022f0bb8b029b
Maximilian Fischer <boehnsins@gmail.com>
2023-01-03 13:38:38 +0100

    feat: update device in database

--- src/database/database.go
+++ src/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	. "longevity/src/model"
@@ -53,6 +54,24 @@ func AddDeviceToDatabase(d *Device) {
 	log.Printf("Inserted device %s!\n", d.Name)
 }
 
+func UpdateDevice(macAddress string, d *Device) {
+	err := checkMatchingMacAdress(macAddress, d)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err := sql.Open("sqlite3", "./longevity.db")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	statement, _ := db.Prepare("update devices set name=?, twin=?, version=? where macAddress=?")
+	statement.Exec(d.Name, d.Twin, d.Version, macAddress)
+	log.Println("Successfully updated the device in database!")
+}
+
 func ReadTable(name string) {
 	db, err := sql.Open("sqlite3", "./longevity.db")
 
@@ -79,4 +98,11 @@ func setup() {
 	}
 	file.Close()
 	log.Println("longevity.db created")
+}
+
+func checkMatchingMacAdress(address string, d *Device) error {
+	if address != d.MacAddress {
+		return errors.New("MacAdresses do not match")
+	}
+	return nil
 }
--- src/database/database_test.go
+++ src/database/database_test.go
@@ -3,7 +3,23 @@ package database
 import (
 	. "longevity/src/model"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+var sample = &Device{"Foo", "00:11:22:33:44", "general", "0.0.1"}
+
+func Test_MatchingMacAddressRaisesError(t *testing.T) {
+	assert := assert.New(t)
+	err := checkMatchingMacAdress("11:22:33:44:55", sample)
+	assert.Error(err)
+}
+
+func Test_matchingMacAddressSucceeds(t *testing.T) {
+	assert := assert.New(t)
+	err := checkMatchingMacAdress("00:11:22:33:44", sample)
+	assert.Nil(err)
+}
 
 func Test_AddEntryToDatabase(t *testing.T) {
 	if testing.Short() {
@@ -14,5 +30,8 @@ func Test_AddEntryToDatabase(t *testing.T) {
 	Start()
 	AddDeviceToDatabase(&sample)
 	AddDeviceToDatabase(&sample2)
+	ReadTable("devices")
+	sample2.Name = "Bar2"
+	UpdateDevice("11:22:33:44:55", &sample2)
 	ReadTable("devices")
 }
