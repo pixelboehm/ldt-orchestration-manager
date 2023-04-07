902043cca230260111bbb57c421173982a142cfb
Maximilian Fischer <boehnsins@gmail.com>
2023-04-07 14:44:27 +0200

    chore(cleanup): remove unused typedefs

--- src/ldt-orchestrator/github/github.go
+++ src/ldt-orchestrator/github/github.go
@@ -13,16 +13,6 @@ import (
 	"golang.org/x/oauth2"
 )
 
-type Asset struct {
-	Name               string `json:"name"`
-	BrowserDownloadURL string `json:"browser_download_url"`
-}
-
-type Release struct {
-	TagName string  `json:"tag_name"`
-	Assets  []Asset `json:"assets"`
-}
-
 type GithubDiscoverer struct {
 	client        *github.Client
 	authenticated bool
