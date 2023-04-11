fb75a7f423b3abb16d8a8aca83febc7a185cc1a0
Maximilian Fischer <boehnsins@gmail.com>
2023-04-11 12:06:43 +0200

    feat(types): add String function for LDT and LDTList type

--- src/types/types.go
+++ src/types/types.go
@@ -1,6 +1,9 @@
 package types
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 type LDT struct {
 	Name    string
@@ -20,4 +23,18 @@ func NewLDTList() *LDTList {
 		LDTs: nil,
 		Lock: sync.Mutex{},
 	}
+}
+
+func (l *LDT) String() string {
+	return fmt.Sprintf("%s \t %s \t %s \t %s \t %s", l.Name, l.Version, l.Os, l.Arch, l.Url)
+}
+
+func (ll *LDTList) String() string {
+	var result string = "Name \t Version \t OS \t Arch \t URL\n"
+	for _, ldt := range ll.LDTs {
+		result += fmt.Sprintf("%s \t %s \t %s \t %s \t %s\n",
+			ldt.Name, ldt.Version, ldt.Os, ldt.Arch, ldt.Url)
+	}
+
+	return result
 }
