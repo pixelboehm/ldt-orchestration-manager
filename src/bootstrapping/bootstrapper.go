00c8af1bf6f70aa2ce1ddf66cf56b876643254c1
Maximilian Fischer <boehnsins@gmail.com>
2023-05-21 12:57:13 +0200

    feat(bootstrapper): handle json from device

--- src/bootstrapping/bootstrapper.go
+++ src/bootstrapping/bootstrapper.go
@@ -1,8 +1,12 @@
 package bootstrapping
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	comms "longevity/src/communication"
+
+	. "longevity/src/database"
 	"net/http"
 )
 
@@ -23,5 +27,13 @@ func (b *Bootstrapper) Run(port int) {
 
 func registration(w http.ResponseWriter, r *http.Request) {
 	log.Println("Bootstrapper: A new registration request came")
-	w.Write([]byte("hello new device"))
+
+	var device Device
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&device); err != nil {
+		log.Println("Bootstrapper: Decoding Error: ", err)
+	}
+	defer r.Body.Close()
+	var result string = fmt.Sprintf("New Device: %s\t%s\t%s\n", device.Name, device.MacAddress, device.Version)
+	w.Write([]byte(result))
 }
